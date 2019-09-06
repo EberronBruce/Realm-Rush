8eb30c29112065951cd08e2b384e3a04a5ce1ca0
Bruce Burgess <bruce@redravencomputing.com>
2019-09-05 19:32:51 -0600

    Added Dictionary, And top Colors of blocks.

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Waypoint : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	Vector2Int gridPos;
+
+	const int gridSize = 10;
+
+	public int GetGridSize() {
+		return gridSize;
+	}
+
+	public Vector2Int GetGridPos() {
+		return new Vector2Int(
+		Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+		Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+		);
+	}
+
+	public void SetTopColor(Color color) {
+		MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+		topMeshRenderer.material.color = color;
+	}
+
 }
