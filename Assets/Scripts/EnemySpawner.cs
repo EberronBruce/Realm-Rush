3893ee8c5012c3a1dfa556118de538c6ef4e2db4
Bruce Burgess <bruce@redravencomputing.com>
2019-09-09 21:56:28 -0600

    Add UI elements and counters

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
 
 	[Range(0.1f, 120f)]
 	[SerializeField] float secondsBetweenSpawns = 5f;
 	[SerializeField] EnemyMovement enemyPrefab;
+	[SerializeField] Text spawnEnemies;
+
+	int score = 0;
 
 	// Start is called before the first frame update
 	void Start() {
 		StartCoroutine(SpawnEnemy());
+		spawnEnemies.text = score.ToString();
 	}
 
 	IEnumerator SpawnEnemy() {
 		
 		while(true) {   // forever
 			Instantiate(enemyPrefab, transform.position, Quaternion.identity, gameObject.transform);
+			AddScore();
 			yield return new WaitForSeconds(secondsBetweenSpawns);
 		}
-		
+
+	}
+
+	private void AddScore() {
+		score++;
+		spawnEnemies.text = score.ToString();
 	}
 }
